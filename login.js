4a43478e3f2259325989ab8e6da38ed233801f59
LambdaConnecta <andrew.ebhoma1@gmail.com>
2026-02-04 21:08:41 +0000

    Add files via upload

---
+++ login.js
@@ -0,0 +1,11 @@
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Sign in to 100%Care</h1>
+      <input placeholder="Email" />
+      <input placeholder="Password" type="password" />
+      <button>Login</button>
+    </div>
+  )
+}
